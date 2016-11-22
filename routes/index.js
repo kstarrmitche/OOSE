dcce0b2952475ada18fe224479807dbdd1122925
Gmario95 <grasselli.mario95@gmail.com>
2016-11-22 15:42:27 -0500

    Fixed Profile

--- routes/index.js
+++ routes/index.js
@@ -121,8 +121,9 @@ function runQuery_content(req, res, client, done, next) {
 
 router.get('/ad',loggedIn,function(req, res, next){
       // connect DB and read table assignments
-	  if(req.user.type != 'ad')
-		  res.redirect('/profile');
+	  if(req.user.type != 'ad'){
+		  res.redirect('/');
+	  }
       pg.connect(process.env.DATABASE_URL + "?ssl=true", connectDB_ad(req,res,next));
 
 });
