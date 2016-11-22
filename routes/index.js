6fae2b8b67429d3ae04e1c77e6f579414d832b1f
Gmario95 <grasselli.mario95@gmail.com>
2016-11-22 16:04:44 -0500

    Fixed Profile

--- routes/index.js
+++ routes/index.js
@@ -80,7 +80,7 @@ function connectDB_profile(req, res, next) {
       console.log(err);
       return next(err);
     }
-    client.query('SELECT location FROM  WHERE username=$1', [req.user.username], runQuery_profile(req, res, client, done, next))
+    client.query('SELECT * FROM  WHERE username=$1', [req.user.username], runQuery_profile(req, res, client, done, next))
   };
 }
 
