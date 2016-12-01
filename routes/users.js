79b58a0d8c6f6108231c610a5e8291efab20b902
Gmario95 <grasselli.mario95@gmail.com>
2016-12-01 15:02:54 -0500

    Removed router.post for ./upload and DB_Insert function

--- routes/users.js
+++ routes/users.js
@@ -155,30 +155,4 @@ router.post('/signup', function(req, res, next) {
 
   });
 
-router.post('/upload', function(req, res, next) {
-	pg.connect(process.env.DATABASE_URL + "?ssl=true", uploadVideo(req,res,next));
-});
-
-function uploadVideo(req, res, next){
-	return function(err, client, done){
-		if(err){
-			console.log("Unable to connect to database");
-			return next(err);
-		}
-		
-		console.log("Upload video");
-		var thisDate = new Date();
-		client.query('INSERT INTO videos (videoTitle, author, videoURL, tags, uploadDate) VALUES($1, $2, $3 {$4, $5, $6} $7)', [req.body.videoTitle, req.user, req.body.videoURL, req.body.tag1, req.body.tag2, req.body.tag2, thisDate], function(err,result){
-			done();
-			if(err){
-				console.log("Unable to query INSERT");
-				return next(err);
-			}
-			console.log("Video upload successful")
-			
-			res.redirect('/profile');
-	});
-}
-  
-  
 module.exports = router;
