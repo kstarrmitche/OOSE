55f2a78e3ffeb9add7290414918b5f7b299e7cb4
Kathy Starr <KStarrMitche@student.bridgew.edu>
2016-12-08 01:35:27 -0500

    layout stuff to make it look a little less ghetto cause I'm bored and I can't do OS stuff anymore

--- app.js
+++ app.js
@@ -89,8 +89,9 @@ app.use(passport.session());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//lets us use static files in public
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
